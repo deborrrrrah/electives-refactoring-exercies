1757d7e7d0984cc900e920493baa2ddb2efc3ebd
Ajeng Sugiarti <ajeng.sugiarti@go-jek.com>
2021-01-02 04:56:39 +0000

    Revert "Add order item initialize and cancel methods"
    This reverts commit 37b226abffa260cf1db35d1d4a40591f66961b48

--- lib/refactoring/order_item.rb
+++ lib/refactoring/order_item.rb
@@ -1,19 +1,5 @@
 module Refactoring
   class OrderItem
-    attr_reader :item, :quantity, :subtotal
-
-    def initialize(item, quantity)
-      if item.stock < quantity
-        raise "Stock unavailable"
-      else
-        @item = item
-        item.stock = item.stock - quantity
-      end
-      @subtotal = (item.price * quantity) + (item.price * item.tax_in_percent)
-    end
-
-    def cancel
-      @item.stock = @item.stock + quantity
-    end
-  end
+    attr_reader :item, :quantity
+  end  
 end
