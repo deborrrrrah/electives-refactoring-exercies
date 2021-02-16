d57f46840ed2ad1db7d35767c64b8743824c9375
deborrrrrah <dajtpbl@gmail.com>
2021-02-17 00:33:00 +0700

    [Debo] Add Order#calculate_price return 33350 when voucher is true, tax is 0.1, delivery_cost is 2000 and add the rest of the comments

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -11,21 +11,27 @@ RSpec.describe Refactoring::Order do
         ]
       end
 
-      it "return when voucher is false, tax is 0.1, delivery_cost is 2000" do
+      it "return 90000 when voucher is false, tax is 0.1, delivery_cost is 2000" do
         @order.order_items << Refactoring::OrderItem.new(@items[0], 2)
         @order.order_items << Refactoring::OrderItem.new(@items[1], 3)
         result = @order.calculate_price(false, 2000, 0.1)
         expect(result).to eq(90000)
       end
 
-      it "return when voucher is true, tax is 0.1, delivery_cost is 2000" do
+      it "return 217600 when voucher is true, tax is 0.1, delivery_cost is 2000" do
         @order.order_items << Refactoring::OrderItem.new(@items[0], 11)
         @order.order_items << Refactoring::OrderItem.new(@items[1], 6)
         result = @order.calculate_price(true, 2000, 0.1)
         expect(result).to eq(217600)
       end
 
-      it "return when voucher is true, tax is not defined, delivery_cost is 2000" do
+      it "return 33350 when voucher is true, tax is 0.1, delivery_cost is 2000" do
+        @order.order_items << Refactoring::OrderItem.new(@items[0], 3)
+        result = @order.calculate_price(true, 2000, 0.1)
+        expect(result).to eq(33350)
+      end
+
+      it "return 198000 when voucher is true, tax is not defined, delivery_cost is 2000" do
         @order.order_items << Refactoring::OrderItem.new(@items[0], 11)
         @order.order_items << Refactoring::OrderItem.new(@items[1], 6)
         result = @order.calculate_price(true, 2000)
