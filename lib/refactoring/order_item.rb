1a4899d6d6a93fa912c6aa6eb51432f906e1337c
deborrrrrah <dajtpbl@gmail.com>
2021-02-16 18:00:00 +0700

    [Debo] Refactor OrderItem#calculate_price

--- lib/refactoring/order_item.rb
+++ lib/refactoring/order_item.rb
@@ -7,21 +7,25 @@ module Refactoring
       @quantity = quantity
     end
 
-    def calculate_price(voucher)
-      if voucher == true
-        if @quantity > 10
-          price = (@quantity * @item.price) * 0.8
-        elsif @quantity > 5
-          price = (@quantity * @item.price) * 0.9
-        elsif @quantity > 2
-          price = (@quantity * @item.price) * 0.95
-        else
-          price = @quantity * @item.price
-        end
-      else
-        price = @quantity * @item.price
-      end
-      price
+    def calculate_discount_price(rate)
+      (@quantity * @item.price) * rate
     end
+
+    def calculate_price(voucher)
+      if voucher == true && @quantity > 2
+        if @quantity > 10
+          rate = 0.8
+        elsif @quantity > 5
+          rate = 0.9
+        elsif @quantity > 2
+          rate = 0.95
+        end
+        calculate_discount_price(rate)
+      else
+        @quantity * @item.price
+      end
+    end
+
+    private :calculate_discount_price
   end
 end
